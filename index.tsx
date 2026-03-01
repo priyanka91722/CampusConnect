a707d53613b3a7f27f5fb7d65c884cb61934a3c3
Priyanka Chavan <priyankachavan220406@gmail.com>
2026-03-01 23:27:29 +0530

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Link href="about">Welcome to CampusConnect!</Link>
+            <Link href="about">Welcome to CampusConnect!</Link>
+            <Link href="about">Welcome to CampusConnect!</Link>
+            <Link href="about">Welcome to CampusConnect!</Link>
+            <Link href="about">Welcome to CampusConnect!</Link>
+        </View>
+    )
+}
